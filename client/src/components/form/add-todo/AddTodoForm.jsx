dc7f38baec0c7e889beae7c4976024ae5a838f2f
zzc0de <6apmuk@gmail.com>
2023-01-05 15:25:21 +0300

    migration addtodoform tohook form done(girty)

--- client/src/components/form/add-todo/AddTodoForm.jsx
+++ client/src/components/form/add-todo/AddTodoForm.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { useForm, Controller } from "react-hook-form";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -24,83 +25,15 @@ const AddTodoForm = ({ create }) => {
     control,
     reset,
     formState: { errors },
-    handleSubmit,
+    handleSubmit, 
     watch,
     setError,
   } = useForm({
     mode: "onBlur",
   });
 
-  // const [errors, setErrors] = useState(
-  //   {
-  //     title: "",
-  //     starttime: "",
-  //     endtime: "",
-  //   }
-  // );
-  // const [validForm, setValidForm] = useState(false);
+  const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   if (todo.title !== ""
-  //   && todo.description !== ""
-  //   && todo.startTime !== ""
-  //   && todo.endTime !== "") {
-  //     setValidForm(true)
-  //   } else {
-  //     setValidForm(false)
-  //   }
-  // }, [todo.title, todo.description, todo.startTime, todo.endTime]);
-
-  // const validate = (name, value) => {
-  //   const checkRegExp = new RegExp(/^[a-zа-яё]+$|\s/i).test(value);
-  //   switch (name) {
-  //     case "title":
-  //       !checkRegExp
-  //         ? setErrors({...errors, title: "Укажите корректный заголовок"})
-  //         : setErrors({...errors, title: ""})
-  //       break;
-  //     case "description":
-  //       !checkRegExp
-  //         ? setErrors({...errors, description: "Укажите корректное описание"})
-  //         : setErrors({...errors, description: ""})
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // }
-
-  // const handleChange = (e) => {
-  //   const {name, value} = e.target;
-  //   validate(name, value)
-  //   setTodo({...todo, [name]: value})
-  // }
-
-  // const handleStartTime = (date) => {
-  //   setTodo({...todo, startTime: date})
-  // }
-
-  // const handleEndTime = (date) => {
-  //   setTodo({...todo, endTime: date})
-  // }
-
-  // const addTodoHandler = () => {
-  //    const newTodo = {
-  //     title: todo.title,
-  //     description: todo.description,
-  //     status: "inprocess",
-  //     startTime: todo.startTime,
-  //     endTime: todo.endTime,
-  //   };
-  //   create(newTodo);
-  //   setTodo({
-  //     id: "",
-  //     title: "",
-  //     description: "",
-  //     status: "",
-  //     startTime: "",
-  //     endTime: "",
-  //   })
-  // };
 
   const onSubmit = (data) => {
     const newTodo = {
@@ -108,14 +41,14 @@ const AddTodoForm = ({ create }) => {
       title: data.title,
       description: data.description,
       status: "inprocess",
-      startTime: data.startTime,
-      endTime: data.endTime,
+      startTime: data.starttime,
+      endTime: data.endtime,
     }
-    console.log(newTodo)
+    create(newTodo)
   };
 
   return (
-    <form className="add-todo-form" onSubmit={handleSubmit(onSubmit)}>
+    <form  className="add-todo-form" onSubmit={handleSubmit(onSubmit)}>
       <div>
         <input
           placeholder="name of task"
@@ -149,18 +82,27 @@ const AddTodoForm = ({ create }) => {
             },
           })}
         />
+       <div className="form-error">
+        {errors.starttime && <p>{errors.starttime.message || "Error"}</p>}
+      </div>
+      <Controller
+      control={control}
+      name="starttime"
+      {...register("starttime", {
+        required: "start time is requied",
+        minLength: {
+          value: 3,
+          message: formConstants.minLengthOfDisplayName,
+        },
+      })}
+      render={({ field: {onChange, value}}) => (
         <DatePicker
-        name="starttime"
-        control={control}
-        {...register("starttime", {
-          required: "requireddd!"
-        })}
-        value={todo.startTime}
-        selected={todo.startTime}
-        // onChange={(date) => handleStartTime(date)}
+        value={Date.parse(todo.startTime)}
+        selected={value}
+        onChange={onChange}
         selectsStart
-        startDate={todo.startTime}
-        endDate={todo.endTime}
+        startDate={Date.parse(todo.startTime)}
+        endDate={Date.parse(todo.endTime)}
         className="date-input"
         placeholderText="Дата начала"
         showTimeSelect
@@ -169,31 +111,37 @@ const AddTodoForm = ({ create }) => {
         dateFormat="Pp"
         timeCaption="time"
         locale={ru}
+        />
+      )}
       />
-      <DatePicker
+
+      <Controller
         control={control}
         name="endtime"
-        value={todo.endTime}
-        selected={todo.endTime}
-        // onChange={(date) => handleEndTime(date)}
-        selectsEnd
-        startDate={todo.startTime}
-        endDate={todo.endTime}
-        minDate={todo.startTime}
-        className="date-input"
-        placeholderText="Дата завершения"
-        showTimeSelect
-        timeFormat="p"
-        timeIntervals={15}
-        dateFormat="Pp"
-        timeCaption="time"
-        locale={ru}
-        {...register("endtime", {
-        })}
+        render={({ field: { onChange, value} }) => (
+        <DatePicker
+          value={Date.parse(todo.endTime)}
+          selected={value}
+          onChange={onChange}
+          selectsEnd
+          startDate={Date.parse(todo.startTime)}
+          endDate={Date.parse(todo.endTime)}
+          minDate={Date.parse(todo.startTime)}
+          className="date-input"
+          placeholderText="Дата завершения"
+          showTimeSelect
+          timeFormat="p"
+          timeIntervals={15}
+          dateFormat="Pp"
+          timeCaption="time"
+          locale={ru}
+        />
+        )}
       />
+    
       
 
-      
+
       {/* {errors.title && <div className="form-error">{errors.title}</div>}
       <input
         placeholder="Название задачи"
