09ea8832acc639650c60b277c0c0b3035a15d424
zzc0de <6apmuk@gmail.com>
2023-02-10 13:34:53 +0300

    admin side inprocess

---
+++ client/src/components/pages/Admin/ChangeRole.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ChangeRole() {
+  return (
+    <div className="block">
+        <div className="block__title">
+            Изменить роль пользователя
+        </div>
+    </div>
+  )
+}
