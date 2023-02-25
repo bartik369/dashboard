413f510200ae3b376221cb6254092074eb5b2b10
zzc0de <6apmuk@gmail.com>
2023-02-25 14:57:32 +0300

    2

--- client/src/store/api/authApi.js
+++ client/src/store/api/authApi.js
@@ -5,29 +5,57 @@ export const authApi = createApi({
     reducerPath: "authApi",
     baseQuery: fetchBaseQuery({ baseUrl: ENV.HOSTNAME }),
     endpoints: (builder) => ({
+        signup: builder.mutation({
+            query: (body) => ({
+                url: "",
+                method: "post",
+                body,
+            }),
+        }),
         signin: builder.mutation({
-                query: ({ email, password }) => {
-                    return {
-                        url: "api/login",
-                        method: "POST",
-                    }
-                }
-            })
-            // checkValidToken: builder.query({
-            //     query: () => ({
-            //         baseUrl: "/api/auth",
-            //         prepareHeaders: (headers, { getState }) => {
-            //             const token = getState().auth.token
-
-        //             if (token) {
-        //                 headers.set('authorization', `Bearer ${token}`)
-        //             }
-
-        //             return headers
-        //         },
-        //     })
-        // })
-    })
-})
+            query: (body) => ({
+                url: "api/login",
+                method: "post",
+                body,
+            }),
+        }),
+        ressetPassword: builder.mutation({
+            query: (body) => ({
+                url: "api/reset",
+                method: "post",
+                body,
+            }),
+        }),
+        setPassword: builder.mutation({
+            query: (body) => ({
+                url: "api/reset",
+                method: "post",
+                body,
+            }),
+        }),
+        ressetPassword: builder.mutation({
+            query: (body) => ({
+                url: "api/reset",
+                method: "post",
+                body,
+            }),
+        }),
+    }),
+});
 
 export const { useSigninMutation } = authApi;
+
+// checkValidToken: builder.query({
+//     query: () => ({
+//         baseUrl: "/api/auth",
+//         prepareHeaders: (headers, { getState }) => {
+//             const token = getState().auth.token
+
+//             if (token) {
+//                 headers.set('authorization', `Bearer ${token}`)
+//             }
+
+//             return headers
+//         },
+//     })
+// })
