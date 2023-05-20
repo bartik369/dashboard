6e6bc90b72ea63abb10b0223e10ba854c3f2dbd7
zzc0de <6apmuk@gmail.com>
2023-05-20 11:41:52 +0300

    start back refactoring

--- client/src/components/widgets/OverdueTodo.jsx
+++ client/src/components/widgets/OverdueTodo.jsx
@@ -14,7 +14,6 @@ import "../timer/timer.css";
 
 const OverdueTodo = () => {
 
-  // const {todos} = useSelector(state => state.todos);
   const {data = [], isLoading} = useGetTodosQuery();
   
   const user = useSelector(selectCurrentUser);
