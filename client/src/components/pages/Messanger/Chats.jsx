14c4b519f4b086f7975f2954140b90359e4c4b04
zzc0de <6apmuk@gmail.com>
2023-05-29 17:34:33 +0300

    messenger set active for first contact done

--- client/src/components/pages/Messanger/Chats.jsx
+++ client/src/components/pages/Messanger/Chats.jsx
@@ -1,23 +1,56 @@
-import React, {useEffect} from 'react';
-import { useSelector } from 'react-redux';
-import { selectCurrentUser } from '../../../store/features/auth/authSlice';
-import { useGetChatsQuery } from '../../../store/features/messenger/messengerApi';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../../store/features/auth/authSlice";
+import { useGetChatsQuery } from "../../../store/features/messenger/messengerApi";
+import "./messenger.css";
 
 export default function Chats() {
-  const user = useSelector(selectCurrentUser)
-  const {data: chats, isLoading} = useGetChatsQuery(user.email)
+  const user = useSelector(selectCurrentUser);
+  const { data: chats, isLoading } = useGetChatsQuery(user.email);
+  const [activeChat, setActiveChat] = useState({
+    id: 0,
+    email: ""
+  })
 
   useEffect(() => {
-    console.log(chats)
-  }, [chats])
-  
+    chats && chats.map((email, index) => {
+
+      if (index === 0) {
+        setActiveChat({
+          ...activeChat,
+          id: index,
+          email: email,
+        })
+      }
+    })
+  }, [chats]);
+
+  console.log(activeChat)
+
+  const activeChatHandler = (email, index) => {
+    setActiveChat({
+      ...activeChat,
+      id : index,
+      email: email,
+    })
+  };
+
   return (
     <div>
-        <ul>
-          {chats && chats.map((item) => {
-           
+      <ul className="chats__items">
+        {chats &&
+          chats.map((email, index) => {
+            return (
+              <li
+                className={`chats__item ${activeChat.id == index && 'active__chat'}`}
+                key={index}
+                onClick={() => activeChatHandler(email, index)}
+              >
+                {email}
+              </li>
+            );
           })}
-        </ul>
+      </ul>
     </div>
-  )
+  );
 }
--- client/src/components/pages/Messanger/Messages.jsx
+++ client/src/components/pages/Messanger/Messages.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 function Messages() {
+
+  const someHandler = (e) => {
+    e.preventDefault()
+    console.log(e.targe)
+  }
   return (
     <div>
-        Тут будет ваш чат
+      <div className="messages__items">
+        список сообщений
+      </div>
+      <div className="messages__input">
+        <textarea onClick={(e) => someHandler(e)}/>
+        <button className="send-btn">Send</button>
+      </div>
+        
     </div>
   )
 }
